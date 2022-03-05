2f4f6589517399d47066c02ab499b07d4c633162
Malu Nadarajan <Malu.Nadarajan@gmail.COM>
2022-03-05 22:17:18 +0530

    No header logo in home page

--- source/insights-web-app/src/app/app-routing.module.ts
+++ source/insights-web-app/src/app/app-routing.module.ts
@@ -2,12 +2,45 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { HomePageComponent } from './layout/home-page/home-page.component';
+import { AboutUsComponent } from './layout/about-us/about-us.component';
+import { ServicesPageComponent } from './layout/services-page/services-page.component';
+import { CustomersComponent } from './layout/customers/customers.component';
+import { ContactsComponent } from './layout/contacts/contacts.component';
+
+const AppRoutesStrings = {
+  home: '',
+  about: 'about',
+  services: 'services',
+  customers: 'customers',
+  contact: 'contact',
+}
 
 const routes: Routes = [
   {
-    path: '',
-    component: HomePageComponent
-  }
+    path: AppRoutesStrings.home,
+    component: HomePageComponent,
+    data: { showLogo: false }
+  },
+  {
+    path: AppRoutesStrings.about,
+    component: AboutUsComponent,
+    data: { showLogo: true }
+  },
+  {
+    path: AppRoutesStrings.services,
+    component: ServicesPageComponent,
+    data: { showLogo: true }
+  },
+  {
+    path: AppRoutesStrings.customers,
+    component: CustomersComponent,
+    data: { showLogo: true }
+  },
+  {
+    path: AppRoutesStrings.contact,
+    component: ContactsComponent,
+    data: { showLogo: true }
+  },
 ];
 
 @NgModule({
--- source/insights-web-app/src/app/layout/header/nav-bar/nav-bar.component.ts
+++ source/insights-web-app/src/app/layout/header/nav-bar/nav-bar.component.ts
@@ -1,4 +1,6 @@
+import { IPageConfig } from './../../../models/page-config';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav-bar',
@@ -7,9 +9,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavBarComponent implements OnInit {
 
-  constructor() { }
+  public showLogo = false;
+
+  constructor(
+    private readonly _activedRoute: ActivatedRoute,
+    private readonly _router: Router
+  ) { }
 
   ngOnInit(): void {
+    this._router.events.subscribe(change => {
+      
+      var currentUrl = change['url'];
+      console.log('url' + currentUrl)
+
+      // Don't show logo for home route ('')
+      if(currentUrl) {
+        this.showLogo = currentUrl !== '/';
+      }
+      
+    })
   }
 
 }
